0caecbf8f44d1e6f2bbd062ecc06d2ae9958f7aa
kimo2508 <kimo2508@gmail.com>
2026-03-31 09:07:28 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = 'https://rijeueugeylqkjvobxaq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpamV1ZXVnZXlscWtqdm9ieGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyODg4OTAsImV4cCI6MjA4ODg2NDg5MH0.LiXxIqIGoEzI4pXF7m5IFxF8vdea46DfPyFIj-d-Ccs';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  global: {
+    headers: {
+      'x-app-token': 'fiji-trip-2026',
+    },
+  },
+});
